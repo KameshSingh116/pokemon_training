e62c5453576f3ab034f1777db121956918daa6c9
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-02-03 18:30:37 +0530

    changes

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 #Radhe Radhe 
-num=int(input("Enter the number of pokemion:"))
-powers=[]
-for i in range(num):
-    power=int(input("Enter the power of pokemon:"))
-    powers.append(power)
+choice='y'
+while(choice=='y'):
+       num=int(input("Enter the number of pokemion:"))
+       powers=[]
+       for i in range(num):
+           power=int(input("Enter the power of pokemon:"))
+           powers.append(power)
 
-mini, maxi=0,0
+       mini, maxi=0,0
 
-for power in powers:
-    if mini==0 and maxi==0:
-        mini,maxi=powers[0],powers[0]
-        print(mini,maxi)
+       for power in powers:
+           if mini==0 and maxi==0:
+               mini,maxi=powers[0],powers[0]
+               print(mini,maxi)
 
-    else:
-        mini=min(mini,power)
-        maxi=max(maxi,power)
-        print(mini,maxi)
+           else:
+              mini=min(mini,power)
+              maxi=max(maxi,power)
+              print(mini,maxi)
+       choice=input("Do you want to continue?(y/n):")
+       if choice=='y':
+           continue
+       else:
+           break
